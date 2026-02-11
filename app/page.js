d4fbe98baca7a6e60f65fedc2fa164ab456a4579
shwarzbergzelda <shwarzbergzelda@gmail.com>
2026-02-11 13:41:41 -0500

    use Next.js to run React

---
+++ app/page.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+function Header({ title }) {
+    return <h1>{title ? title : 'Default Title'}</h1>;
+}
+
+export default function HomePage() {
+    const [likes, setLikes] = useState(0);
+
+    const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+
+    function handleClick() {
+        setLikes(likes + 1);
+    }
+
+    return (
+        <div>
+            <Header title="Develop. Preview. Ship." />
+            <ul>
+                {names.map((name) => (
+                    <li>{name}</li>
+                ))}
+            </ul>
+            <button onClick={handleClick}>Like({likes})</button>
+        </div>
+    );
+}
